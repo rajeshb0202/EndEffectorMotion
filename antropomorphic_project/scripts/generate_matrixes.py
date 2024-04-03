6580a0615f5c9accf7e9ff8c9a445d2da5550cc8
rajesh <rajeshb0202@gmail.com>
2024-04-03 15:22:09 +0000

    modified the generate_matrixes program in the OOPS style.

--- antropomorphic_project/scripts/generate_matrixes.py
+++ antropomorphic_project/scripts/generate_matrixes.py
@@ -1,68 +1,68 @@
 #!/usr/bin/env python3
 
-from sympy import Matrix, cos, sin, Symbol, simplify, trigsimp, nsimplify
+from sympy import Matrix, cos, sin, Symbol, simplify, trigsimp, nsimplify, preview
 from sympy.interactive import printing
-
 
 # To make display prety
 printing.init_printing(use_latex = True)
 
-theta_i = Symbol("theta_i")
-alpha_i = Symbol("alpha_i")
-r_i = Symbol("r_i")
-d_i = Symbol("d_i")
+class GenerateHomogeneousMatrix:
 
-DH_Matric_Generic = Matrix([[cos(theta_i), -sin(theta_i)*cos(alpha_i), sin(theta_i)*sin(alpha_i), r_i*cos(theta_i)],
-                            [sin(theta_i), cos(theta_i)*cos(alpha_i), -cos(theta_i)*sin(alpha_i), r_i*sin(theta_i)],
-                            [0, sin(alpha_i), cos(alpha_i), d_i],
+    def __init__(self):
+        self.theta_i = Symbol("theta_i")
+        self.alpha_i = Symbol("alpha_i")
+        self.r_i = Symbol("r_i")
+        self.d_i = Symbol("d_i")
+        self.DH_Matric_Generic = Matrix([[cos(self.theta_i), -sin(self.theta_i)*cos(self.alpha_i), sin(self.theta_i)*sin(self.alpha_i), self.r_i*cos(self.theta_i)],
+                            [sin(self.theta_i), cos(self.theta_i)*cos(self.alpha_i), -cos(self.theta_i)*sin(self.alpha_i), self.r_i*sin(self.theta_i)],
+                            [0, sin(self.alpha_i), cos(self.alpha_i), self.d_i],
                             [0,0,0,1]])
 
-result_simpl = simplify(DH_Matric_Generic)
-
-from sympy import preview
 
 
-# Now create A01, A12, A23
-
-theta_1 = Symbol("theta_1")
-theta_2 = Symbol("theta_2")
-theta_3 = Symbol("theta_3")
-
-alpha_1 = 1.57
-alpha_2 = 0.0
-alpha_3 = 0.0
-
-r_1 = 0.0
-r_2 = Symbol("r_2")
-r_3 = Symbol("r_3")
-
-d_1 = 0.0
-d_2 = 0.0
-d_3 = 0.0
-
-A01 = DH_Matric_Generic.subs(r_i,r_1).subs(alpha_i,alpha_1).subs(d_i,d_1).subs(theta_i, theta_1)
-A12 = DH_Matric_Generic.subs(r_i,r_2).subs(alpha_i,alpha_2).subs(d_i,d_2).subs(theta_i, theta_2)
-A23 = DH_Matric_Generic.subs(r_i,r_3).subs(alpha_i,alpha_3).subs(d_i,d_3).subs(theta_i, theta_3)
-
-A03 = A01 * A12 * A23
+    def generate_matrix(self, frame_params):
+        Aij = self.DH_Matric_Generic.subs(self.r_i,frame_params['r']).subs(self.alpha_i,frame_params['alpha']).subs(self.d_i,frame_params['d']).subs(self.theta_i, frame_params['theta'])
+        Aij = trigsimp(Aij, method='fu')
+        return Aij
+    
+    def generate_matrix_simplified(self, Aij_matrix):
+        Aij_simplified = simplify(Aij_matrix)
+        return Aij_simplified
+        
 
 
+# initialising the DH parameters to generate HOMOGENEOUS MATRIX
+frame01_params = {'theta': Symbol("theta_1"),
+                  'alpha': 1.57,
+                   'r': 0.0,
+                   'd': 0.0}
 
-A01 = trigsimp(A01, method='fu').evalf(1)
-A12 = trigsimp(A12, method='fu')
-A23 = trigsimp(A23, method='fu')
-A03 = trigsimp(A03, method='fu').evalf(1)
+frame12_params = {'theta': Symbol("theta_2"),
+                  'alpha': 0.0,
+                  'r': Symbol("r_2"),
+                  'd': 0.0}
 
-A03_simplify = simplify(A03).evalf(1)
-
-# A03_test_simplified = simplify(A03_simplify)
-
-
-preview(A01, viewer='file', filename="A0_1.png", dvioptions=['-D','300'])
-preview(A12, viewer='file', filename="A1_2.png", dvioptions=['-D','300'])
-preview(A23, viewer='file', filename="A2_3.png", dvioptions=['-D','300'])
-preview(A03, viewer='file', filename="A0_3.png", dvioptions=['-D','300'])
-preview(A03_simplify, viewer='file', filename="A0_3_simplified.png", dvioptions=['-D','300'])
+frame13_params = {'theta': Symbol("theta_3"),
+                  'alpha': 0.0,
+                  'r': Symbol("r_3"),
+                  'd': 0.0}
 
 
-print("Homogeneous matrix for all frames generated")
+def computeMatrix():
+    GHM = GenerateHomogeneousMatrix()
+    A01 = GHM.generate_matrix(frame01_params)
+    A12 = GHM.generate_matrix(frame12_params)
+    A23 = GHM.generate_matrix(frame13_params)
+    A03 = A01 * A12 * A23
+    A03_simplify = GHM.generate_matrix_simplified(A03).evalf(1)
+    
+    # save the generated matrixes to local files
+    preview(A01.evalf(1), viewer='file', filename="A0_1.png", dvioptions=['-D','300'])
+    preview(A12, viewer='file', filename="A1_2.png", dvioptions=['-D','300'])
+    preview(A23, viewer='file', filename="A2_3.png", dvioptions=['-D','300'])
+    preview(A03.evalf(1), viewer='file', filename="A0_3.png", dvioptions=['-D','300'])
+    preview(A03_simplify, viewer='file', filename="A0_3_simplified.png", dvioptions=['-D','300'])
+    print("Homogeneous matrix for all frames generated")
+
+if __name__ == "__main__":
+    computeMatrix()
